4ebcec8217a81418d62f5a01a7ee30dadda10a11
Corbin Petzinger <corbin.petzinger@gmail.com>
2025-12-03 13:29:01 +0000

    Yayy

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+//java is a strongly typed language;  you have to define a viriable with its data type
+
+class DataTypeExamples {
+    int myInt = 50;
+    double myDouble = 67.676767;
+    char myChar = 'Q';
+    boolean myBool = false;
+
+}
+
+
+public class DataTypes 
+{
+    public static void main(String[] args) {
+        
+
+        DataTypeExamples dt = new DataTypeExamples();
+        System.out.print("myint: " + dt.myInt);
+    }
+
+}
