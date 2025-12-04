bccb0f7627cbdbb9c0c2a13c76a99fae5055aee7
Corbin Petzinger <corbin.petzinger@gmail.com>
2025-12-04 14:38:50 +0000

    POop

---
+++ Variables.java
@@ -0,0 +1,36 @@
+// Two types of variabes: instance variables and static variables
+// variables use camelCase 
+// constants use uppercase
+
+class Student {
+
+    String studentName; // instance variable, belongs to object
+    static int studentCount = 0; // static variable, belongs to class
+    static final String SCHOOL_NAME = "CSAEA"; // final statics in snake case
+
+    Student(String n) {
+        studentName = n; //belong to specific object
+        System.out.println(n + " is a student at " + SCHOOL_NAME );
+
+        studentCount++;
+
+        System.out.println("Number of students: " + studentCount);
+    }
+    
+}
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        new Student("Alex");
+         new Student("poop");
+          new Student("bolbus");
+           new Student("patricia");
+            new Student("parenthestes");
+             new Student("Oculorus");
+              new Student("Brizilig");
+
+    }
+
+}
