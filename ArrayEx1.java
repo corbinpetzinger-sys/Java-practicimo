95fffa7f92b3b9aab0045994f05e1413c88a320a
Corbin Petzinger <corbin.petzinger@gmail.com>
2026-01-07 14:38:37 +0000

    new commit

---
+++ ArrayEx1.java
@@ -0,0 +1,20 @@
+public class ArrayEx1 {
+
+    public static void main(String args[]) {
+        
+        int[] primeNum = new int[23];
+        int[] even = new int[6];
+        long[] primes = new long[20];
+        primes[0] = 2;
+        primes[1] = 3;
+        long[] primes2 = primes;
+        System.out.println(primes2[0]);
+        primes2[0] = 5;
+        System.out.println(primes[0]);
+        int[] score = new int[9];
+        byte[] b = {1,1,1,1,1,1,1,1,1,1};
+
+
+        }
+    }
+
